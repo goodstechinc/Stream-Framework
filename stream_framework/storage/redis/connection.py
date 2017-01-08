aaeec9f2024b265c22fce20af6df2b8e8846e10d
Developer <developer@developer.local>
2017-01-07 16:42:53 -0800

    Changing decoded_responses flag to False in Redis->Storage->Connection.

--- stream_framework/storage/redis/connection.py
+++ stream_framework/storage/redis/connection.py
@@ -29,7 +29,7 @@ def setup_redis():
             port=config['port'],
             password=config.get('password'),
             db=config['db'],
-            decode_responses=True
+            decode_responses=False
         )
         pools[name] = pool
     return pools
